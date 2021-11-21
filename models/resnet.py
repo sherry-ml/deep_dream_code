f8de5b49bf5ea2db8fbe82be6d7d40adbfa49743
sherry-ml <67177106+sherry-ml@users.noreply.github.com>
2021-11-21 16:53:38 +0530

    Update resnet.py

--- models/resnet.py
+++ models/resnet.py
@@ -14,14 +14,21 @@ import torch.nn.functional as F
 class BasicBlock(nn.Module):
     expansion = 1
 
-    def __init__(self, in_planes, planes, stride=1):
+    def __init__(self, in_planes, planes, stride=1, dropout):
         super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv1 = nn.Sequential(nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False),
+                                   nn.BatchNorm2d(planes),
+                                   nn.ReLU(inplace=True),
+                                   nn.Dropout2d(dropout)
+                                  )
+        #self.conv1 = nn.Conv2d(
+         #   in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        #self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Sequential(nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False),
+                                   nn.BatchNorm2d(planes),
+                                   nn.ReLU(inplace=True),
+                                   nn.Dropout2d(dropout)
+                                  )
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
@@ -32,21 +39,26 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
+        #out = F.relu(self.bn1(self.conv1(x)))
+        #out = self.bn2(self.conv2(out))
+        out =  self.conv1(x)
+        out = self.conv2(out)
         out += self.shortcut(x)
         out = F.relu(out)
         return out
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, dropout, num_classes=10):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Sequential(nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                   nn.BatchNorm2d(64),
+                                   nn.ReLU(inplace=True),
+                                   nn.Dropout2d(dropout)
+                                  )
+        
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
@@ -57,12 +69,13 @@ class ResNet(nn.Module):
         strides = [stride] + [1]*(num_blocks-1)
         layers = []
         for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
+            layers.append(block(self.in_planes, planes, stride, dropout))
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
+        #out = F.relu(self.bn1(self.conv1(x)))
+        out = self.conv1(x)
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
@@ -70,11 +83,11 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
-        return out
+        return F.log_softmax(out,dim=1)
 
 
 def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+    return ResNet(BasicBlock, [2, 2, 2, 2], dropout)
 
 
 def ResNet34():
