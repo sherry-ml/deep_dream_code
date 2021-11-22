57e2618a661c5e15a8afc0fccba5f62f7b7aaec8
sherry-ml <67177106+sherry-ml@users.noreply.github.com>
2021-11-22 10:12:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -281,6 +281,7 @@ def torch_device(dev_stat):
   return device
 
 def view_model_summary(test_model,device):
+  from torchsummary import summary
   test_model = test_model.to(device)
   summary(test_model, input_size=(3, 32, 32))
   print(test_model)
