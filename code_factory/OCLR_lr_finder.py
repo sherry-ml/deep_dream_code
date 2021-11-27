a805845423734f33496a0e73ae9f2adf53fadd2d
sherry-ml <67177106+sherry-ml@users.noreply.github.com>
2021-11-27 17:36:59 +0530

    Update OCLR_lr_finder.py

--- code_factory/OCLR_lr_finder.py
+++ code_factory/OCLR_lr_finder.py
@@ -1,1 +1,16 @@
 
+from torch_lr_finder import LRFinder
+
+def find_lr(self,model,train_loader, test_loader, start_lr, end_lr, epochs):
+  lr_epochs = epochs
+  num_iterations = len(test_loader) * lr_epochs
+  criterion = nn.CrossEntropyLoss()
+  optimizer = optim.SGD(model.parameters(), lr=start_lr, momentum=0.90)
+  lr_finder = LRFinder(model, optimizer, criterion, device="cuda")
+  lr_finder.range_test(train_loader, val_loader=test_loader, end_lr=end_lr, num_iter=num_iterations, step_mode="linear",diverge_th=50)
+        
+  # Plot
+  max_lr = lr_finder.history['lr'][lr_finder.history['loss'].index(lr_finder.best_loss)]
+  # Reset graph
+  lr_finder.reset()
+  return max_lr
