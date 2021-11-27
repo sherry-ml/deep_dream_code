94dfa4031a00b011a436a1fb009c40c8638f819a
sherry-ml <67177106+sherry-ml@users.noreply.github.com>
2021-11-27 19:06:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -92,7 +92,7 @@ def test(model, device, test_loader,test_losses, test_acc,epoch):
     test_acc.append(100. * correct / len(test_loader.dataset))
     return accuracy_epoch
   
-def train_test_model(optim, oclr, model, trainloader, testloader, norm_type='BN', EPOCHS=20, max_epoch, lr=0.001, lrmax=0.5, device='cpu'):
+def train_test_model(optim, oclr, model, trainloader, testloader, norm_type='BN', EPOCHS=20, max_epoch=5, lr=0.001, lrmax=0.5, device='cpu'):
 
   train_losses_BN = []
   test_losses_BN = []
