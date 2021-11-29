ab0ac890c13f06ace8208e1e3c20d335ec0f653f
sherry-ml <67177106+sherry-ml@users.noreply.github.com>
2021-11-29 08:01:51 +0530

    Update tune_lr_train_model.py

--- code_factory/tune_lr_train_model.py
+++ code_factory/tune_lr_train_model.py
@@ -6,6 +6,7 @@ import numpy as np
 from torch_lr_finder import LRFinder
 from deep_dream_code.models.custom_resnet import Custom_ResNet
 from deep_dream_code.code_factory.OCLR_lr_finder import find_lr
+from deep_dream_code.main import train, test, train_test_model
 
 def pipeline_tune_lr_train_model(list_end_lr, trainloader_mod, testloader_mod, total_epoch, max_epoch,  opt, oclr, device):
   for lr in list_end_lr:
